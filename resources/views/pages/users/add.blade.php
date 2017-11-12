76f9f5a8b7cb1d526b8f8ad53cd735d8021324ce
Tobi Amira <amiratobi@gmail.com>
2017-11-13 00:37:57 +0100

    Change styling for add users view

--- resources/views/pages/users/add.blade.php
+++ resources/views/pages/users/add.blade.php
@@ -3,38 +3,41 @@
 @section ('content')
 	<div class="containter">
 		<div class="row">
-			<div class="col mt-5 ">
-				<h4 class="text-center text-primary">Add User</h4>
-				<div class="col-12 col-sm-10 col-md-8 ml-auto mr-auto border pt-4 pb-4">
+			<div class="col mt-2">
+				<h2 class="text-center display-4 text-dark">Add User</h2>
+				<div class="col-12 col-sm-8 col-md-6 ml-auto mr-auto border pt-4 pb-4 add-user-form-cont rounded">
 					<p class="text-center text-dark">Enter the user's information below</p>
 					<form>
 						<div class="row">
-							<div class="col-12 col-md-6 mt-2 mb-2">
+							<div class="col-12 mt-2 mb-2">
 								<input type="text" class="form-control" placeholder="First name" name="firstName">
 							</div>
-							<div class="col-12 col-md-6  mt-2 mb-2">
+							<div class="col-12 mt-2 mb-2">
 								<input type="text" class="form-control" placeholder="Last name" name="lastName">
 							</div>
 						</div>
+						<hr>
 						<div class="row">
-							<div class="col-12 col-md-6 mt-2 mb-2">
+							<div class="col-12 mt-2 mb-2">
 								<input type="text" class="form-control" placeholder="Email Address" name="email">
 							</div>
-							<div class="col-12 col-md-6 mt-2 mb-2">
+							<div class="col-12 mt-2 mb-2">
 								<input type="text" class="form-control" placeholder="Phone e.g. (08092238281)" name="phoneNumber">
 							</div>
 						</div>
+						<hr>
 						<div class="row">
-							<div class="col-12 col-md-6 mt-2 mb-2">
+							<div class="col-12 mt-2 mb-2">
 								<input type="text" class="form-control" placeholder="Username" name="username">
 							</div>
-							<div class="col-12 col-md-6 mt-2 mb-2">
+							<div class="col-12 mt-2 mb-2">
 								<input type="password" class="form-control" name="password" placeholder="password">
 							</div>
 						</div>
+						<hr>
 						<div class="row">
-							<div class="col text-center mt-4">
-								<button class="btn btn-success float-right  pl-5 pr-5">Submit</button>
+							<div class="col text-center mt-2">
+								<button class="btn btn-success float-right pl-5 pr-5">Submit</button>
 							</div>
 						</div>
 					</form>
