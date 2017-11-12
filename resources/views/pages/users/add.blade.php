60e51a132b3336daa618cb6ec2b8ada958e1c53a
Tobi Amira <amiratobi@gmail.com>
2017-11-13 00:26:59 +0100

    Complete add user view

--- resources/views/pages/users/add.blade.php
+++ resources/views/pages/users/add.blade.php
@@ -3,9 +3,41 @@
 @section ('content')
 	<div class="containter">
 		<div class="row">
-			<div class="col">
-				<div class="col-12 col-sm-10 col-md-8 ml-auto mr-auto border mt-5 pt-4 pb-4">
-					
+			<div class="col mt-5 ">
+				<h4 class="text-center text-primary">Add User</h4>
+				<div class="col-12 col-sm-10 col-md-8 ml-auto mr-auto border pt-4 pb-4">
+					<p class="text-center text-dark">Enter the user's information below</p>
+					<form>
+						<div class="row">
+							<div class="col-12 col-md-6 mt-2 mb-2">
+								<input type="text" class="form-control" placeholder="First name" name="firstName">
+							</div>
+							<div class="col-12 col-md-6  mt-2 mb-2">
+								<input type="text" class="form-control" placeholder="Last name" name="lastName">
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-12 col-md-6 mt-2 mb-2">
+								<input type="text" class="form-control" placeholder="Email Address" name="email">
+							</div>
+							<div class="col-12 col-md-6 mt-2 mb-2">
+								<input type="text" class="form-control" placeholder="Phone e.g. (08092238281)" name="phoneNumber">
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-12 col-md-6 mt-2 mb-2">
+								<input type="text" class="form-control" placeholder="Username" name="username">
+							</div>
+							<div class="col-12 col-md-6 mt-2 mb-2">
+								<input type="password" class="form-control" name="password" placeholder="password">
+							</div>
+						</div>
+						<div class="row">
+							<div class="col text-center mt-4">
+								<button class="btn btn-success float-right  pl-5 pr-5">Submit</button>
+							</div>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
