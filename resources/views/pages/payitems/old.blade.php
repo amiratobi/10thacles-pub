a104bfb46484aae2a1fe9511a76340197bb3ddeb
Tobi Amira <tobiamira@Tobis-MacBook.local>
2017-11-21 21:21:46 +0100

    backup old payitems view

--- resources/views/pages/payitems/old.blade.php
+++ resources/views/pages/payitems/old.blade.php
@@ -7,6 +7,9 @@
 			<div class="col-12 pt-2">
                 <h1 class="display-4 text-center">Pay Items</h1>
                 
+                <table class="table">
+                    
+                </table>
             </div>
         </div>
     </div>
